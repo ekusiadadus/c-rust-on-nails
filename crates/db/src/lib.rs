ac83f78134360ca66a52e1e988f8340ccc521bfc
ekusiadadus <ekusiadadus@gmail.com>
2024-04-06 17:13:47 +0000

    feat: add db migrate

---
+++ crates/db/src/lib.rs
@@ -0,0 +1,68 @@
+use std::str::FromStr;
+use std::sync::Arc;
+
+pub use cornucopia_async::Params;
+pub use deadpool_postgres::{Pool, PoolError, Transaction};
+use rustls::client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier};
+use rustls_pki_types::{CertificateDer, ServerName, UnixTime};
+pub use tokio_postgres::Error as TokioPostgresError;
+
+pub use queries::users::User;
+
+pub fn create_pool(database_url: &str) -> Pool {
+    let config = tokio_postgres::Config::from_str(database_url).unwrap();
+
+    let manager = if config.get_ssl_mode() != tokio_postgres::config::SslMode::Disable {
+        let tls_config = rustls::ClientConfig::builder()
+            .dangerous()
+            .with_custom_certificate_verifier(Arc::new(DummyTlsVerifier))
+            .with_no_client_auth();
+
+        let tls = tokio_postgres_rustls::MakeRustlsConnect::new(tls_config);
+        deadpool_postgres::Manager::new(config, tls)
+    } else {
+        deadpool_postgres::Manager::new(config, tokio_postgres::NoTls)
+    };
+
+    Pool::builder(manager).build().unwrap()
+}
+
+#[derive(Debug)]
+struct DummyTlsVerifier;
+
+impl ServerCertVerifier for DummyTlsVerifier {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &CertificateDer,
+        _intermediates: &[CertificateDer],
+        _server_name: &ServerName,
+        _ocsp_response: &[u8],
+        _now: UnixTime,
+    ) -> Result<ServerCertVerified, rustls::Error> {
+        Ok(ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        _message: &[u8],
+        _cert: &CertificateDer,
+        _dss: &rustls::DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        _message: &[u8],
+        _cert: &CertificateDer,
+        _dss: &rustls::DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
+        Vec::new()
+    }
+}
+
+include!(concat!(env!("OUT_DIR"), "/cornucopia.rs"));
