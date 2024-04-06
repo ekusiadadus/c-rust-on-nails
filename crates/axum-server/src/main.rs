65c41e3fb3b9889f9de5ba6f78b90a63638e4855
ekusiadadus <ekusiadadus@gmail.com>
2024-04-06 17:35:42 +0000

    feat: pin version

--- crates/axum-server/src/main.rs
+++ crates/axum-server/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod errors;
+
+use crate::errors::CustomError;
+use axum::{extract::Extension, response::Json, routing::get, Router};
+use db::User;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let config = config::Config::new();
+
+    let pool = db::create_pool(&config.database_url);
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(users))
+        .layer(Extension(config))
+        .layer(Extension(pool.clone()));
+
+    // run it
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("listening on {}", addr);
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn users(Extension(pool): Extension<db::Pool>) -> Result<Json<Vec<User>>, CustomError> {
+    let client = pool.get().await?;
+
+    let users = db::queries::users::get_users().bind(&client).all().await?;
+
+    Ok(Json(users))
 }
